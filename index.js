9b5aa1a96fd4011b5089dc5083f68e6201e702ff
pulkit07092001 <55887735+pulkit07092001@users.noreply.github.com>
2022-11-25 00:16:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(8800, () => {
+app.listen(8000, () => {
   console.log("Backend server is running!");
 });
