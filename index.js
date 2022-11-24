6b5be4d39b740d6802bc9e966cc99253c0afe5c0
pulkit07092001 <55887735+pulkit07092001@users.noreply.github.com>
2022-11-25 00:55:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
@@ -13,7 +13,7 @@ const router = express.Router();
 const path = require("path");
 
 dotenv.config();
-// app.use(cors);
+app.use(cors);
 mongoose.connect(
   process.env.MONGO_URL,
   { useNewUrlParser: true, useUnifiedTopology: true },
